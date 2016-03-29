bc641fe2a946225269a52318be0273a0110c2bc2
Stéphane Brunner <stephane.brunner@camptocamp.com>
2016-03-29 12:04:39 +0200

    Fix the readme source

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = (
-    open(os.path.join(here, 'README.rst')).read()
+    open(os.path.join(here, 'README.md')).read()
 )
 
 install_requires = [
